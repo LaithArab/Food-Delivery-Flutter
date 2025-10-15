3548f9acf58691bca68f458b325adaf48f33de34
Laith Test <laitharab746@gmail.com>
2025-10-15 19:56:06 +0300

    make app more responsive

--- lib/pages/account_page.dart
+++ lib/pages/account_page.dart
@@ -28,47 +28,94 @@ class AccountPage extends StatelessWidget {
     required IconData icon,
   }) {
     final size = MediaQuery.of(context).size;
+    final isLandScape =
+        MediaQuery.of(context).orientation == Orientation.landscape;
     return ListTile(
-      leading: Icon(icon, size: size.height * 0.03),
-      title: Text(title),
+      leading: Icon(
+        icon,
+        size: isLandScape ? size.height * 0.13 : size.height * 0.06,
+      ),
+      title: Text(
+        title,
+        style: TextStyle(
+          fontSize: isLandScape ? size.height * 0.07 : size.height * 0.03,
+        ),
+      ),
       subtitle: subtitle != null ? Text(subtitle) : null,
-      trailing: const Icon(Icons.chevron_right, size: 25),
+      trailing: Icon(
+        Icons.chevron_right,
+        size: isLandScape ? size.height * 0.13 : size.height * 0.05,
+      ),
       onTap: () => print('$title clicked!'),
+    );
+  }
+
+  Widget personPhoto({required double height, required double width}) {
+    return Container(
+      height: height,
+      width: width,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        image: DecorationImage(
+          image: AssetImage('assets/images/laithPhoto.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-
-    return Center(
+    final isLandScape =
+        MediaQuery.of(context).orientation == Orientation.landscape;
+    final namePerson = Text(
+      "Laith Arab",
+      style: Theme.of(
+        context,
+      ).textTheme.headlineMedium!.copyWith(fontWeight: FontWeight.w600),
+    );
+    return SingleChildScrollView(
       child: Column(
         children: [
-          Container(
-            height: size.height * 0.3,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              image: DecorationImage(
-                image: AssetImage('assets/images/laithPhoto.jpg'),
-                fit: BoxFit.cover,
-              ),
+          if (!isLandScape) ...[
+            personPhoto(height: size.height * 0.3, width: size.width * 0.7),
+            SizedBox(height: size.height * 0.02),
+            namePerson,
+            SizedBox(height: size.height * 0.02),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                orderVoucherITem(context, name: "Order", number: 50),
+                orderVoucherITem(context, name: "Vouchers", number: 10),
+              ],
             ),
-          ),
-          SizedBox(height: size.height * 0.02),
-          Text(
-            "Laith Arab",
-            style: Theme.of(
-              context,
-            ).textTheme.headlineMedium!.copyWith(fontWeight: FontWeight.w600),
-          ),
-          SizedBox(height: size.height * 0.02),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              orderVoucherITem(context, name: "Order", number: 50),
-              orderVoucherITem(context, name: "Vouchers", number: 10),
-            ],
-          ),
+          ],
+          if (isLandScape) ...[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    personPhoto(
+                      height: size.height * 0.5,
+                      width: size.width * 0.4,
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    namePerson,
+                    SizedBox(height: size.height * 0.06),
+                    orderVoucherITem(context, name: "Order", number: 50),
+                    SizedBox(height: size.height * 0.02),
+                    orderVoucherITem(context, name: "Vouchers", number: 10),
+                  ],
+                ),
+              ],
+            ),
+          ],
           SizedBox(height: size.height * 0.03),
           Divider(),
 
